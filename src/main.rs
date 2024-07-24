d85ad26f4cf872872c2b5e50250bf4b013139604
Konstantin Freidlin <freidlinks@cloudx.group>
2024-07-24 13:32:28 +0300

    Minor improvement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// src: https://doc.rust-lang.ru/book/ch02-00-guessing-game-tutorial.html
+
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
@@ -9,9 +11,12 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Plese input your guess.");
+
+    let mut guess_count: u32 = 0;
     
     loop {
         let mut guess = String::new();
+        guess_count += 1;
         io::stdin()
             .read_line(&mut guess)
             .expect("Fialed to read line :(");
@@ -23,7 +28,7 @@ fn main() {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win. You guessed it on the {guess_count} attempt!");
                 break;
             },
         }
